623b998ae10eff98afb77a0247ca00572c55b1f1
Johannes <johannes.humml@outlook.com>
2021-10-09 16:05:41 +0200

    uart test

--- MountainViewer/src/main.cpp
+++ MountainViewer/src/main.cpp
@@ -1,10 +1,11 @@
 #include <Arduino.h>
 
 void setup() {
-  // put your setup code here, to run once:
-  // asdfjhsalkdfhajksdhfkas
+  Serial.begin(9600);
 }
 
 void loop() {
+
   // put your main code here, to run repeatedly:
+  Serial.print("ddffd");
 }
