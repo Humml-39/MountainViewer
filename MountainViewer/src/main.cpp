c3b6e3fddf9e772e8ae3870df2db319e5e0d75a3
Jussel-d <daniel.jussel@student.htl-rankweil.at>
2021-10-11 13:52:30 +0200

    k

--- MountainViewer/src/main.cpp
+++ MountainViewer/src/main.cpp
@@ -6,7 +6,7 @@ void setup() {
 
 
 void loop(){
-  623b998ae10eff98afb77a0247ca00572c55b1f1
+  //623b998ae10eff98afb77a0247ca00572c55b1f1
   // put your main code here, to run repeatedly:
   Serial.print("ddffd");
 }
