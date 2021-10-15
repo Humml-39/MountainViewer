33fb6e5403c0e3590b0b2d31998d451c9ece076b
Johannes <johannes.humml@outlook.com>
2021-10-15 15:43:25 +0200

    EEPROM testprogramm

---
+++ Mountains2EEPROM/src/main.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+//Program to write the Mountain Data to the EEPROM
+//by Johannes Humml
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
