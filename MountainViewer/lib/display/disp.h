fb1c450dcdf611536e784c11a5ceef45608e6612
Jussel-d <daniel.jussel@student.htl-rankweil.at>
2021-11-15 14:01:06 +0100

    Display+Splash fertig

--- MountainViewer/lib/display/disp.h
+++ MountainViewer/lib/display/disp.h
@@ -1,8 +1,8 @@
 #ifndef DISP_H
     #define DISP_H 
-
+    
     extern void testdrawchar(char Textsize,char cursor1, char cursor2, String buchstabe);
-
+    extern void setup2();
 #endif
 
 
--- MountainViewer/src/main.cpp
+++ MountainViewer/src/main.cpp
@@ -1,11 +1,11 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include <Wire.h>
-#include
+#include <Disp.h>
 
 
 void setup() {
-
+  setup2();
   testdrawchar(2,0,0,"Spar spar");      // Draw characters of the default font
   
 }
