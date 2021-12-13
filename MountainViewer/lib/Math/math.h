50607ec09aac9ebdffb8e7fd749bceef5a102bba
Jussel-d <daniel.jussel@student.htl-rankweil.at>
2021-12-13 16:59:05 +0100

    Berechnungen lib start

---
+++ MountainViewer/lib/Math/math.h
@@ -0,0 +1,14 @@
+#ifndef MATH_H
+    #define MATH_H 
+    
+    typedef struct 
+    {
+        char *name;
+        double phi;
+        double abstand;
+        int height;
+    }mountain_new;
+     
+    extern mountain_new mountains_new[];
+    
+#endif
