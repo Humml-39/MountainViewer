48c9f841be399da352d470fa517d8c662fe91d5a
Jussel-d <daniel.jussel@student.htl-rankweil.at>
2022-01-08 16:45:08 +0100

    Berechnung fertig

--- MountainViewer/src/Mymath.cpp
+++ MountainViewer/src/Mymath.cpp
@@ -9,29 +9,63 @@ void auswertung(double angel);
 mountain_new mountains_new[];
 
 void new_array(double X_Me,double Y_Me){
+    Serial.print("\n");
     double abstand;
     double phi;
     double X_Berg;
     double Y_Berg;
-    int k =0,ks=0;
+    int k =0;
+    double temp0,temp1;
+
     for (int i=0;i<854;i++){
         X_Berg = mountains[i].posX;
         Y_Berg = mountains[i].posY;
         abstand = sqrt(pow(abs(X_Berg-X_Me),2)+pow(abs(Y_Berg-Y_Me),2));
         if(abstand<0.1){
-            phi = asin(abs(X_Berg-X_Me)/2*abstand);
+            if((Y_Berg-Y_Me)>0){
+                if ((X_Berg-X_Me) > 0){
+                //Quadrant 1
+                temp0 = (X_Berg-X_Me);
+                temp1 = -(Y_Berg-Y_Me);
+                phi = 90 + atan(temp0 / temp1) * (180 / 3.14159);
+                }
+                else
+                {
+                //Quadrant 2
+                temp0 = -(X_Berg-X_Me);
+                temp1 = -(Y_Berg-Y_Me);
+                phi = 90 - atan(temp0 / temp1) * (180 / 3.14159);
+                }
+            }
+            else {
+                if ((X_Berg-X_Me) < 0)
+                {
+                //Quadrant 3
+                temp0 = -(X_Berg-X_Me);
+                temp1 = (Y_Berg-Y_Me);
+                phi = 270 + atan(temp0 / temp1) * (180 / 3.14159);
+                }
+                else
+                {
+                //Quadrant 4
+                temp0 = (X_Berg-X_Me);
+                temp1 = (Y_Berg-Y_Me);
+                phi = 270 - atan(temp0 / temp1) * (180 / 3.14159);
+                }
+            }
             mountains_new[k].abstand=abstand;
             mountains_new[k].phi=phi;
             mountains_new[k].height=mountains[i].height;
             mountains_new[k].name=mountains[i].name;
-            //Serial.print(mountains_new[k].name);
             k++;
-        } 
-        ks++;      
+        }       
     }
-    Serial.print(mountains[5].posX);
-    Serial.print("\n");
-    Serial.print(ks);
+    for(int i=0;i<k;i++){
+        Serial.print(mountains_new[i].name);
+        Serial.print("  ");
+        Serial.print(mountains_new[i].phi);
+        Serial.print("\n");
+    }
 }
 
 void auswertung(double angel){
