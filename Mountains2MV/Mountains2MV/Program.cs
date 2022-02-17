b861ff7b0d08414a1a1747a41ab5e1c29a3757fc
Johannes <johannes.humml@outlook.com>
2022-02-17 12:11:51 +0100

    mountains2mv improved + commented

--- Mountains2MV/Mountains2MV/Program.cs
+++ Mountains2MV/Mountains2MV/Program.cs
@@ -9,39 +9,46 @@ namespace Mountains2MV
     {
         public static void Main()
         {
+            //Startgruß
             Console.WriteLine("Welcome to Mountain2MV");
             Console.WriteLine("----------------------");
-            int length = 0;
-            Console.WriteLine("Data Source (CSV):");
+
+            int length = 0; //Anzahl aller Berge
+
+            Console.WriteLine("Data Source (CSV):"); //Welche Datei?
             string path1 = Console.ReadLine();
 
-            Console.WriteLine("finished reading CSV");
-            try
-            {
-                var reader = new StreamReader(File.OpenRead(@path1));
 
+            try //Prüfen ob die Eingabe richitg verläuft
+            {
+                var reader = new StreamReader(File.OpenRead(@path1)); //Öffnen der CSV
+                Console.WriteLine("Path OK");
+
+                //Liste der aller Bergnamen, -höhen und Position (Längen- und Breitengrad)
                 List<string> name = new List<string>();
                 List<string> lon = new List<string>();
                 List<string> lat = new List<string>();
                 List<string> alt = new List<string>();
+
                 while (!reader.EndOfStream)
                 {
-                    var line = reader.ReadLine();
-                    var values = line.Split(',');
+                    var line = reader.ReadLine(); //Auslesen der Zeile
+                    var values = line.Split(','); //Absppeichern der Werte auf die Variable, trennung durch ","
 
-                    name.Add(values[0]);
-                    lon.Add(values[1]);
-                    lat.Add(values[2]);
-                    alt.Add(values[3]);
+                    name.Add(values[0]); //Bergname wird hinzugefügt
+                    lon.Add(values[1]); //Längengrad wird hinzugefügt
+                    lat.Add(values[2]); //Breitengrad wird hinzugefügt
+                    alt.Add(values[3]); //Höhe wird hinzugefügt
 
-                    length++;
+                    length++; //Die Anzahl der Berge wird inkrementiert
                 }
-                Console.WriteLine("Where to Save:");
-                string path = Console.ReadLine() + "\\mountains.cpp";
-                //string path = @"D:\mountains.cpp";
 
-                StreamWriter writer = new StreamWriter(@path);
+                Console.WriteLine("Where to Save:"); //Speicherort abfrage
+                string path = Console.ReadLine() + "\\mountains.cpp"; //Speicherort+Name
 
+                StreamWriter writer = new StreamWriter(@path); //Erstellen einer neuen Datei
+
+                //Ab hier: schreibt C++-Code....
                 writer.WriteLine("#include \"mountains.h\"");
                 writer.WriteLine("#include \"arduino.h\"");
                 writer.WriteLine("");
@@ -49,21 +56,23 @@ namespace Mountains2MV
                 writer.WriteLine("");
                 writer.WriteLine("mountain mountains[] = {");
 
-                for (int i = 0; i < length; i++)
+                for (int i = 0; i < length; i++) //Schleife für jeden Berg
                 {
                     writer.WriteLine("    {\"" + name[i] + "\"," + lat[i] + "," + lon[i] + "," + alt[i] + "},");
                 }
 
                 writer.WriteLine("};");
                 writer.Close();
+                //Fertig mit der Datei
 
                 Console.WriteLine("mountains.cpp has been created");
                 Console.ReadKey();
             }
     
-            catch
+            catch //Falls ein Problem auftritt --> Fehlermeldung
             {
-                Console.Write("Invalid Path");
+                Console.WriteLine("Error!");
+                Console.Write("Is the Path correct?");
                 Console.ReadKey();
             }
         }
